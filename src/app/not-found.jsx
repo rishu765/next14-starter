d96238acf413c226e179a1742d5c8a96e7e002d8
rishu1234 <rishabh@catalogus.in>
2024-05-13 20:22:09 +0530

    sample proj

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import  Link from "next/link"
+
+const NotFound = () => {
+  return (
+    <div>
+                  <h2>not found</h2>
+                  <Link href="/">return home</Link>
+    </div>
+  )
+}
+
+export default NotFound
